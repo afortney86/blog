5d6e5b0995398937e28942eaac98baf9c3012ed5
Anthony Fortney <afortney86@gmail.com>
2017-01-04 14:44:10 -0600

    java for roll dice

--- src/main/java/com/codeup/RollDice.java
+++ src/main/java/com/codeup/RollDice.java
@@ -1,7 +1,27 @@
 package com.codeup;
 
-/**
- * Created by anthonyfortney on 1/4/17.
- */
-public class RollDice {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+class RollDice {
+
+    @GetMapping ("/roll-dice")
+    public String welcome(){
+        return "roll-dice";
+    }
+
+    @GetMapping ("/roll-dice/{userGuess}")
+    public String userGuess(@PathVariable int userGuess, Model model) {
+        model.addAttribute("userGuess", userGuess);
+        return "roll-dice";
+
+    }
+
+
+
 }
